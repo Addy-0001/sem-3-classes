64a1ae80fd9f58671194085ac068a082cfc2fdf4
Adamya <adamsneupane@gmail.com>
2024-05-14 11:35:30 +0545

    14 May 2024

--- ProjectAnimation/app/build.gradle.kts
+++ ProjectAnimation/app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.projectanimation"
     compileSdk = 34
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.projectanimation"
         minSdk = 27
--- ProjectAnimation/app/src/main/java/com/example/projectanimation/DashboardActivity.kt
+++ ProjectAnimation/app/src/main/java/com/example/projectanimation/DashboardActivity.kt
@@ -3,15 +3,42 @@ package com.example.projectanimation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.projectanimation.databinding.ActivityDashboardBinding
+import com.example.projectanimation.fragments.HomePageFragment
+import com.example.projectanimation.fragments.MessagePageFragment
+import com.example.projectanimation.fragments.ProfilePageFragment
+import com.example.projectanimation.fragments.SearchPageFragment
 
 class DashboardActivity : AppCompatActivity() {
+
+    lateinit var activityDashboardBinding: ActivityDashboardBinding;
+
+    private fun replaceFragment(fragment : Fragment) {
+        var fragmentManager: FragmentManager = supportFragmentManager;
+        var fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentLayout, fragment)
+        fragmentTransaction.commit()
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_dashboard)
+        activityDashboardBinding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(activityDashboardBinding.root)
 
-
+        activityDashboardBinding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(HomePageFragment())
+                R.id.search -> replaceFragment(SearchPageFragment())
+                R.id.message -> replaceFragment(MessagePageFragment())
+                R.id.profile -> replaceFragment(ProfilePageFragment())
+                else->{}
+            }
+            true
+        }
     }
 }
