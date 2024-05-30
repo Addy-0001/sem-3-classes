d68c90cbec6cc32f6ec63d6ed36b1bca40282341
Adamya <adamsneupane@gmail.com>
2024-05-30 22:00:28 +0545

    added kotlin files

--- KOTLIN-backend-/app/src/main/java/com/example/cruddatabse_firebase/DashBoardActivity.kt
+++ KOTLIN-backend-/app/src/main/java/com/example/cruddatabse_firebase/DashBoardActivity.kt
@@ -3,10 +3,13 @@ package com.example.cruddatabse_firebase
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.cruddatabse_firebase.adapter.ProductAdapter
 import com.example.cruddatabse_firebase.databinding.ActivityDashBoardBinding
 import com.example.cruddatabse_firebase.model.ProductModel
 import com.google.firebase.database.DataSnapshot
@@ -14,36 +17,40 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import kotlin.math.log
 
 class DashBoardActivity : AppCompatActivity() {
-    lateinit var binding:ActivityDashBoardBinding
-    var database : FirebaseDatabase = FirebaseDatabase.getInstance()
-    var ref : DatabaseReference = database.reference.child("products")
+    lateinit var binding: ActivityDashBoardBinding
+    var database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    var ref: DatabaseReference = database.reference.child("products")
+    var productList = ArrayList<ProductModel>();
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityDashBoardBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.floatingActionButton.setOnClickListener{
-            var intent = Intent(this@DashBoardActivity,AddProductActivity::class.java)
+        binding.floatingActionButton.setOnClickListener {
+            var intent = Intent(this@DashBoardActivity, AddProductActivity::class.java)
             startActivity(intent)
         }
 
         ref.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-               for (eachData in snapshot.children) {
-                   var product = eachData.getValue(ProductModel::class.java)
-                   if (product!= null){
-                       Log.d("Data from Firebase : ", product.name)
-                       Log.d("Data from Firebase : ", product.id)
-                       Log.d("Data from Firebase : ", product.price.toString())
-                       Log.d("Data from Firebase : ", product.description)
-
-                   }
-               }
+                for (eachData in snapshot.children) {
+                    var product = eachData.getValue(ProductModel::class.java)
+                    if (product != null) {
+                        Log.d("Data from Firebase : ", product.name)
+                        Log.d("Data from Firebase : ", product.id)
+                        Log.d("Data from Firebase : ", product.price.toString())
+                        Log.d("Data from Firebase : ", product.description)
+                        productList.add(product)
+                    }
+                }
+                var adapter = ProductAdapter(productList)
+                binding.recyclerView.layoutManager = LinearLayoutManager(this@DashBoardActivity)
+                binding.recyclerView.adapter = adapter
             }
+
 
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
